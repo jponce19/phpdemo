5b7b4d15f85d110d7984b2c1a3df531eff31dbad
jponce <contacto.jponce@gmail.com>
2019-08-30 10:22:59 -0400

    correcion de archivo app.yaml

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
-<?php
-    echo "<p> hola mundo index ";
-    echo "<pre>";
-    var_dump($_SERVER); 
-?>    
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "<p> hola mundo index ";
+        echo "<pre>";
+        var_dump($_SERVER);   
+    ?>
+</body>
+</html>
